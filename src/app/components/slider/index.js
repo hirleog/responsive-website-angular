ea0a012ee64274a0b50692bed3481ca7e731db5e
hirleog <87393381+hirleog@users.noreply.github.com>
2022-12-24 14:45:36 -0300

    rick website evolution

---
+++ src/app/components/slider/index.js
@@ -0,0 +1,11 @@
+const left = document.getElementById('left')
+
+function animeScroll(){
+  const windowTop = window.pageYOffset;
+//   left.forEach(function)
+  console.log("aidsjijdaij")
+}
+
+window.addEventListener('scroll', function(){
+  animeScroll();
+})
