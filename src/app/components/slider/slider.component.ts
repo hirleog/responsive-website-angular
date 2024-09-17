33b0d13484da4e197c320f72375f7729885b9469
Hirle <87393381+hirleog@users.noreply.github.com>
2024-09-17 02:10:47 -0300

    new layout for: banner & about

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
+  @ViewChild('bannerVideo') bannerVideo!: ElementRef;
+  isVideoPlaying: boolean = true;  // Flag para controlar se o vídeo está sendo exibido
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  ngAfterViewInit() {
+    const video: HTMLVideoElement = this.bannerVideo.nativeElement;
+
+    // Tenta carregar o vídeo
+    video.load();
+
+    // Tenta reproduzir o vídeo quando estiver pronto
+    video.addEventListener('canplay', () => {
+      video.play().catch(error => {
+        console.error("Erro ao tentar reproduzir o vídeo, substituindo por imagem:", error);
+        this.isVideoPlaying = false;  // Se der erro, substitui o vídeo pela imagem
+      });
+    });
+
+    // Caso o vídeo falhe ao carregar
+    video.addEventListener('error', () => {
+      console.error("Erro ao carregar o vídeo, substituindo por imagem.");
+      this.isVideoPlaying = false;
+    });
+  }
 }
